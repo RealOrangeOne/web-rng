ff57e4af82cc3ef66463543719f02614b8cce384
Jake Howard <git@theorangeone.net>
2020-05-17 15:22:30 +0100

    Show error message when webcrypto isn't available

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -3,5 +3,12 @@ import { h, render } from "preact";
 import App from "./app";
 
 document.addEventListener("DOMContentLoaded", () => {
-  render(<App />, document.getElementById("app"));
+  const target = document.getElementById("app");
+  if (window.crypto) {
+    while (target.hasChildNodes()) {
+      target.removeChild(target.lastChild);
+    }
+
+    render(<App />, target);
+  }
 });
